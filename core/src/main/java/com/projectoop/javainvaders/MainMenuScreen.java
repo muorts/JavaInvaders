81d15f71f5849cada0592517190efff2838797bc
JoaoMarcelo-Faria <joaogcfaria@gmail.com>
2026-06-11 23:31:26 -0300

    Inicio do desenvolvimento aqui

--- core/src/main/java/com/projectoop/javainvaders/MainMenuScreen.java
+++ core/src/main/java/com/projectoop/javainvaders/MainMenuScreen.java
@@ -14,7 +14,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
 
 public class MainMenuScreen implements Screen {
     final JavaInvadersGame game;        // referência ao jogo global
